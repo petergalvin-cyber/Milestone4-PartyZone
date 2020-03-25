5c2d0b3052cccd716a2360091ea8ab4c32237745
petergalvin-cyber <petergalvin@consultant.com>
2020-03-25 11:13:58 +0000

    Include stripe js and keys

--- partyzone/settings.py
+++ partyzone/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from os import path
+
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,5 +140,8 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSGAE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
