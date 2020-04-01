4b0764e4930b4cfee74d122d8e7cdaa2e489388c
petergalvin-cyber <petergalvin@consultant.com>
2020-04-01 14:08:54 +0000

    New host in settings

--- partyzone/settings.py
+++ partyzone/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'),'partysupplies.herokuapp.com']
 
 
 # Application definition
