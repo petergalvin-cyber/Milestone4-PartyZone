b2c8327ff2c2d3242da51abb47960d8eb3a81770
petergalvin-cyber <petergalvin@consultant.com>
2020-04-06 16:54:35 +0000

    Updated aa_products view to return products using selected category and not the default

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from .models import Product, Theme
 
 # Create your views here.
 def all_products(request):
-    products = Product.objects.all().filter(stock__gt=0)
+    products = Product.objects.all().filter(stock__gt=0).filter(category__category__icontains=request.session.get('sel_cat',''))
     categories = Theme.objects.all()
     
     return render(request, "products.html", {"products": products, "categories": categories})
